ba9ce2d00e062a4c427d4262c0489f30cff9b15c
Brano <branislav.kutnik@gmail.com>
2020-11-05 23:50:16 +0100

    Updated configuration possibilities

--- config/config.php
+++ config/config.php
@@ -1,21 +1,32 @@
 <?php
 
-use OutputFormat\Colors\Color;
-use OutputFormat\Formats\Formats;
+use OutputFormat\Formats\Color;
+use OutputFormat\Formats\DecorativeFormats;
+use OutputFormat\Formats\FinalFormatter;
+use OutputFormat\Formats\Text\Lowercase;
 use OutputFormat\Output\Stdout;
 
 return [
     'translation-formats' => [
-        'red' => '\e[31m',
-        'green' => '\e[32m',
-        'yellow' => '\e[33m',
-        'blue' => '\e[34m'
+        'red' => "31",
+        'green' => "32",
+        'yellow' => "33",
+        'blue' => "34",
+        'default-color' => "0"
+    ],
+
+    'escape-tag' => [
+        'begin' => "\033[",
+        'end' => "m",
+        'delimiter' => ";"
     ],
 
     'output' => Stdout::class,
 
     'formatters' => [
         'color' => Color::class,
-        'text-features' => Formats::class
+        'text-features' => DecorativeFormats::class,
+        'final-formatter' => FinalFormatter::class,
+        'lowercase' => Lowercase::class
     ]
 ];
