b5d7deee42eae71cde47d6a8a9c36bb16fcac3d5
Brano <branislav.kutnik@gmail.com>
2020-11-07 23:43:22 +0100

    Added support for 88/256 colors in terminal

--- src/Formats/Color.php
+++ src/Formats/Color.php
@@ -4,6 +4,9 @@ namespace OutputFormat\Formats;
 
 class Color extends DecorativeFormats
 {
+    /** @var string */
+    protected string $configPath = 'colors';
+
     /**
      * @param string $color
      * @return Color
@@ -11,5 +14,21 @@ class Color extends DecorativeFormats
     public function setColor(string $color) :Color
     {
         return $this->setFormat($color);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function translateFormat(string $rawFormat) :string
+    {
+        $replaced = preg_replace('/[^0-9]/', '', $rawFormat);
+        if (!empty($replaced)) {
+            $moreColors = (config(sprintf('translation-formats.%s.more-color-prefix', $this->configPath)));
+
+            return $moreColors . $replaced;
+        }
+        $rawFormat = sprintf('%s.%s', $this->configPath, $rawFormat);
+
+        return parent::translateFormat($rawFormat);
     }
 }
